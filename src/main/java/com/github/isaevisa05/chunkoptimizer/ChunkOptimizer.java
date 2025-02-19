214cc4a13552cb7bff6293e38ac8ae1b86403214
isaevisa05 <isaevisao55@gmail.com>
2025-02-19 09:03:05 +0300

    Create plugin files

---
+++ src/main/java/com/github/isaevisa05/chunkoptimizer/ChunkOptimizer.java
@@ -0,0 +1,17 @@
+package com.github.isaevisa05.chunkoptimizer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChunkOptimizer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
